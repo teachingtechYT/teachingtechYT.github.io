44349f79bf16fd32c92b67b5b81095c038123499
lawgicau <lawgicau@gmail.com>
2021-02-19 09:27:46 +1100

    Round 4 - public

---
+++ js/dynamic.js
@@ -0,0 +1,55 @@
+var pages = ["Home", "Calibration", "Troubleshooting", "Review Policy", "Sponsors", "Contact"]
+var urls = ["index.html", "calibration.html", "troubleshooting.html", "reviewpolicy.html", "sponsors.html", "contact.html"]
+var menu = '<img src="img/ttwhite.png" />';
+for(var i = 0; i < pages.length; i++){
+    menu += '<a href="'+urls[i]+'" target="_self"';
+    if(pages[i] == pageName){
+        menu += ' class="currentPage"';
+    }
+    menu += '>'+pages[i]+'</a>';
+}
+menu += '<span id="donate">Useful? Considering supporting me: <a href="https://paypal.me/testlawgicau" target="blank"><img class="icon" src="img/paypal.png" /></a><a href="http://www.patreon.com/teachingtech" target="blank"><img class="icon" src="img/patreon.png" /></a></span>';
+$('#menu').html(menu);
+function sizeBody(){
+    var menuClearance = $('#menu').outerHeight()+30;
+    $('body').css("margin-top",menuClearance);
+}
+
+$('head').append('<title>'+pageTitle+'</title>');
+var header ='<img src="img/tt.png" class="icon" style="vertical-align:middle;" /><h1>'+pageTitle+'</h1>';
+$('#header').html(header);
+
+var footer = `<p style="text-align: center;">This page is fully <a href="https://github.com/teachingtechYT/teachingtechYT.github.io" target="_blank">open source</a>. If you find a bug or have a feature request, please post in the <a href="https://github.com/teachingtechYT/teachingtechYT.github.io/issues" target="_blank">issues</a> section.</p>
+    <p style="text-align: center;">This page was created using:</p>
+    <ul style="margin-top:-20px;">
+        <li><a href="https://jquery.com/" target="_blank">jQuery</a></li>
+        <li><a href="https://github.com/jellekralt/Responsive-Tabs" target="_blank">Responsive Tabs by jellekraut</a></li>
+        <li><a href="https://fonts.google.com/specimen/Roboto" target="_blank">Roboto font</a></li>
+        <li><a href="https://github.com/tedktedk/videobox" target="_blank">Videobox by tedktedk</a></li>
+        <li><a href="https://github.com/noelboss/featherlight" target="_blank">Featherlight by noelboss</a></li>
+        <li><a href="https://github.com/toperkin/staticFormEmails/blob/master/README.md" target="_blank">staticFormEmails by toperkin</a></li>
+    </ul>`;
+$('#footer').html(footer);
+
+$(document).ready(function(){
+    $('#tabs').responsiveTabs({
+        startCollapsed: 'accordion'
+    });
+    $(".videoThumb").videoBox({
+        width: '480',
+        height: '360',
+        loop: false,
+        autoplay: false,
+        byline: false,
+        color: "00adef",
+        maxheight: '',
+        maxwidth: '',
+        portrait: true,
+        title: '',
+        controls: 1
+    });
+    sizeBody();
+    $(window).resize(function(){
+        sizeBody();
+    });
+  });
---
+++ js/loadscripts.js
@@ -0,0 +1,20 @@
+var scripts = `
+<link  type="text/css" rel="stylesheet" href="css/styles.css">
+<link  type="text/css" rel="stylesheet" href="css/responsive-tabs.css">
+<link  type="text/css" rel="stylesheet" href="css/featherlight.css">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<script src="js/jquery-3.5.1.js"></script>
+<script src="js/jquery.responsiveTabs.js"></script>
+<script src="js/videobox.js"></script>
+<script src="js/featherlight.js"></script>
+<script src="js/firstlayer.js"></script>
+<script src="js/baseline.js"></script>
+<script src="js/retraction.js"></script>
+<script src="js/temperature.js"></script>
+<script src="js/acceleration.js"></script>
+<script src="js/encoding-indexes.js"></script>
+<script src="js/encoding.js"></script>
+<script src="js/gcodeprocessing.js"></script>
+<script src="js/minimumlength.js"></script>
+<script src="js/persist.js"></script>`;
+document.write(scripts);
