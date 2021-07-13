4fc2645bc4fce17e1d8a7baef1822697666ffb5c
lawgicau <lawgicau@gmail.com>
2021-07-13 11:39:53 +1000

    Add M501 to end gcode
    #169
    Avoid the user sending M500 and saving temporary test settings for acceleration, etc.

--- js/commongcode.js
+++ js/commongcode.js
@@ -18,4 +18,5 @@ var commonEnd = `G28 X0 ; home X axis
 M106 S0 ; turn off cooling fan
 M104 S0 ; turn off extruder
 M140 S0 ; turn off bed
-M84 ; disable motors`
+M84 ; disable motors
+M501 ; restore previous EEPROM values`
