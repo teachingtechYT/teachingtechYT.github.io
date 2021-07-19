76af120a2f667cdad7d86c769b18910e1417e75b
lawgicau <lawgicau@gmail.com>
2021-07-19 17:14:28 +1000

    Fixed font and loading optimisation
    GH-26
    Courtesy of KieranHolroyd

--- js/loadscripts.js
+++ js/loadscripts.js
@@ -2,7 +2,7 @@ var scripts = `
 <link  type="text/css" rel="stylesheet" href="css/styles.css">
 <link  type="text/css" rel="stylesheet" href="css/responsive-tabs.css">
 <link  type="text/css" rel="stylesheet" href="css/featherlight.css">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300,400&display=swap" rel="stylesheet">
 <script src="js/jquery-3.5.1.js"></script>
 <script src="js/jquery.responsiveTabs.js"></script>
 <script src="js/videobox.js"></script>
